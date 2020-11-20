a002302c6236947a6cbcfa2448caff70ca9570d6
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2020-11-20 14:56:38 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -122,6 +122,10 @@ LOCATION 's3a://cdp-sandbox-default-se/datalake/warehouse/tablespace/external/hi
 # ORC
 
 
+CREATE EXTERNAL TABLE IF NOT EXISTS `twitterorc` (`msg` STRING, `unixtime` STRING, `friends_count` STRING, `hashtags` STRING, `listed_count` STRING, `tweet_id` STRING, `user_name` STRING, `favourites_count` STRING, `source` STRING, `placename` STRING, `media_url` STRING, `retweet_count` STRING, `user_mentions_name` STRING, `geo` STRING, `urls` STRING, `countrycode` STRING, `user_url` STRING, `place` STRING, `coordinates` STRING, `handle` STRING, `profile_image_url` STRING, `time_zone` STRING, `ext_media` STRING, `statuses_count` STRING, `followers_count` STRING, `location` STRING, `user_mentions` STRING, `user_description` STRING) 
+STORED AS ORC
+LOCATION '/twitterorc'
+
 
 # Test Queries
 

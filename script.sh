410f8c6c4e4bc04862ae6b695c959152e574bfc4
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2020-12-03 23:21:20 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -17,6 +17,9 @@
 
 create table weather, weather_details
 
+CREATE EXTERNAL TABLE IF NOT EXISTS `twitterorc` (`msg` STRING, `unixtime` STRING, `friends_count` STRING, `hashtags` STRING, `listed_count` STRING, `tweet_id` STRING, `user_name` STRING, `favourites_count` STRING, `source` STRING, `placename` STRING, `media_url` STRING, `retweet_count` STRING, `user_mentions_name` STRING, `geo` STRING, `urls` STRING, `countrycode` STRING, `user_url` STRING, `place` STRING, `coordinates` STRING, `handle` STRING, `profile_image_url` STRING, `time_zone` STRING, `ext_media` STRING, `statuses_count` STRING, `followers_count` STRING, `location` STRING, `user_mentions` STRING, `user_description` STRING) STORED AS ORC
+LOCATION 's3a://aceawworkshopuat1/user/tspann/twitterorc'
+
 # Hive streaming table
 # Hive external table
 # 7.2.2 - Data Engineering: Apache Spark, Apache Hive, Apache Oozie

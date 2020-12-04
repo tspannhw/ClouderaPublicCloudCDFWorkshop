e2dacd3137e47074485072e0834effffd938a4db
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2020-12-03 23:59:05 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -12,6 +12,12 @@
 
 # Load Schemas
 
+
+CREATE EXTERNAL TABLE IF NOT EXISTS `weatherorc` (`credit` STRING, `credit_url` STRING, `image` STRUCT<`url`:STRING, `title`:STRING, `link`:STRING>, `suggested_pickup` STRING, `suggested_pickup_period` BIGINT, `location` STRING, `station_id` STRING, `latitude` DOUBLE, `longitude` DOUBLE, `observation_time` STRING, `observation_time_rfc822` STRING, `weather` STRING, `temperature_string` STRING, `temp_f` DOUBLE, `temp_c` DOUBLE, `relative_humidity` BIGINT, `wind_string` STRING, `wind_dir` STRING, `wind_degrees` BIGINT, `wind_mph` DOUBLE, `wind_kt` BIGINT, `pressure_in` DOUBLE, `dewpoint_string` STRING, `dewpoint_f` DOUBLE, `dewpoint_c` DOUBLE, `visibility_mi` DOUBLE, `icon_url_base` STRING, `two_day_history_url` STRING, `icon_url_name` STRING, `ob_url` STRING, `disclaimer_url` STRING, `copyright_url` STRING, `privacy_policy_url` STRING) 
+STORED AS ORC
+LOCATION 's3a://aceawworkshopuat1/user/tspann/weatherorc'
+
+
 # HBase
 # 7.2.2 - Operational Database: Apache HBase, Phoenix-COD-v3
 

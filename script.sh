b53244949b7c5870b9ebd79049faf67ce5462258
Timothy Spann <18673814+tspannhw@users.noreply.github.com>
2020-12-04 00:09:37 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -12,6 +12,18 @@
 
 # Load Schemas
 
+CREATE EXTERNAL TABLE weatherkudu 
+(`location` STRING,`observation_time` STRING, `credit` STRING, `credit_url` STRING, `image` STRING, `suggested_pickup` STRING, `suggested_pickup_period` BIGINT,
+`station_id` STRING, `latitude` DOUBLE, `longitude` DOUBLE,  `observation_time_rfc822` STRING, `weather` STRING, `temperature_string` STRING,
+`temp_f` DOUBLE, `temp_c` DOUBLE, `relative_humidity` BIGINT, `wind_string` STRING, `wind_dir` STRING, `wind_degrees` BIGINT, `wind_mph` DOUBLE, `wind_gust_mph` DOUBLE, `wind_kt` BIGINT,
+`wind_gust_kt` BIGINT, `pressure_string` STRING, `pressure_mb` DOUBLE, `pressure_in` DOUBLE, `dewpoint_string` STRING, `dewpoint_f` DOUBLE, `dewpoint_c` DOUBLE, `windchill_string` STRING,
+`windchill_f` BIGINT, `windchill_c` BIGINT, `visibility_mi` DOUBLE, `icon_url_base` STRING, `two_day_history_url` STRING, `icon_url_name` STRING, `ob_url` STRING, `disclaimer_url` STRING,
+`copyright_url` STRING, `privacy_policy_url` STRING)
+STORED BY 'org.apache.hadoop.hive.kudu.KuduStorageHandler'
+TBLPROPERTIES (
+  "kudu.table_name"="default.weatherkudu", 
+  "kudu.master_addresses"="ace-ww-workshop-rtkudu-master10.ace-aw-w.ylcu-atmi.cloudera.site:7051,ace-ww-workshop-rtkudu-master30.ace-aw-w.ylcu-atmi.cloudera.site:7051, ace-ww-workshop-rtkudu-master20.ace-aw-w.ylcu-atmi.cloudera.site:7051"
+);
 
 CREATE EXTERNAL TABLE IF NOT EXISTS `weatherorc` (`credit` STRING, `credit_url` STRING, `image` STRUCT<`url`:STRING, `title`:STRING, `link`:STRING>, `suggested_pickup` STRING, `suggested_pickup_period` BIGINT, `location` STRING, `station_id` STRING, `latitude` DOUBLE, `longitude` DOUBLE, `observation_time` STRING, `observation_time_rfc822` STRING, `weather` STRING, `temperature_string` STRING, `temp_f` DOUBLE, `temp_c` DOUBLE, `relative_humidity` BIGINT, `wind_string` STRING, `wind_dir` STRING, `wind_degrees` BIGINT, `wind_mph` DOUBLE, `wind_kt` BIGINT, `pressure_in` DOUBLE, `dewpoint_string` STRING, `dewpoint_f` DOUBLE, `dewpoint_c` DOUBLE, `visibility_mi` DOUBLE, `icon_url_base` STRING, `two_day_history_url` STRING, `icon_url_name` STRING, `ob_url` STRING, `disclaimer_url` STRING, `copyright_url` STRING, `privacy_policy_url` STRING) 
 STORED AS ORC
